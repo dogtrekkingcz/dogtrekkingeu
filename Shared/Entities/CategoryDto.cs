f69d123518b3157b01def50720f3d4b8324823cf
Radek Kotesovec <radek.kotesovec@dogtrekking.cz>
2023-03-16 02:13:45 +0100

    .

--- Shared/Entities/CategoryDto.cs
+++ Shared/Entities/CategoryDto.cs
@@ -3,7 +3,11 @@
     public sealed record CategoryDto
     {
         public Guid Id { get; set; }
+
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
         public IList<RacerDto> Racers { get; set; } = new List<RacerDto>();
     }
 }
--- Shared/Mapping/SharedMappingCategory.cs
+++ Shared/Mapping/SharedMappingCategory.cs
@@ -9,7 +9,10 @@ namespace DogtrekkingCz.Shared.Mapping
         public static TypeAdapterConfig AddSharedMappingCategory(this TypeAdapterConfig typeAdapterConfig)
         {
             typeAdapterConfig.NewConfig<CategoryDto, Protos.Shared.Category>()
-                .TwoWays();
+                .Map(d => d.Id, s => s.Id.ToString());
+
+            typeAdapterConfig.NewConfig<Protos.Shared.Category, CategoryDto>()
+                .Map(d => d.Id, s => Guid.Parse(s.Id));
 
             return typeAdapterConfig;
         }
