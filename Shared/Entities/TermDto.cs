94504ca4b32a529e7b20caa787246b96febad192
Radek Kotesovec <radek.kotesovec@dogtrekking.cz>
2023-03-16 01:43:29 +0100

    Term should be not null; Setting it for Now - Now+3D

--- Shared/Entities/TermDto.cs
+++ Shared/Entities/TermDto.cs
@@ -2,8 +2,8 @@
 {
     public sealed record TermDto
     {
-        public DateTimeOffset From { get; set; }
+        public DateTimeOffset From { get; set; } = DateTime.Now;
 
-        public DateTimeOffset To { get; set; }
+        public DateTimeOffset To { get; set; } = DateTime.Now.AddDays(3);
     }
 }
