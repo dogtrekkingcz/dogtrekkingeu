410a159614f2047e78a3e25581e9d7a987c63d15
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-05-15 17:03:36 +0200

    deployment fixing

--- init_scripts/renew_certificate.sh
+++ init_scripts/renew_certificate.sh
@@ -3,10 +3,10 @@ OldSecretName=$(date --date yesterday +%Y-%m-%d)
 DomainName=dogsontrail.eu
 AppName=DogsOnTrailApp
 
-cd /etc/letsencrypt/live/dogsontrail.eu/
 cd /dogsontrail_data/certbot/conf/live/dogsontrail.eu/
 
 openssl pkcs12 -export -out ${DomainName}.pfx -inkey privkey.pem -in cert.pem -certfile fullchain.pem -passin pass: -passout pass:
+chmod 775 /dogsontrail_data/certbot/conf/live/dogsontrail.eu/dogsontrail.eu.pfx
 
 sudo docker secret create $SecretName /dogsontrail_data/certbot/conf/live/dogsontrail.eu/${DomainName}.pfx
 
