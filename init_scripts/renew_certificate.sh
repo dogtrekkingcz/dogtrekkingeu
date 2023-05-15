0f729eefa4a34c5ddaa28042ed57de3eac9aa85c
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-05-15 08:11:29 +0200

    deployment issues fixies

--- init_scripts/renew_certificate.sh
+++ init_scripts/renew_certificate.sh
@@ -4,13 +4,14 @@ DomainName=dogsontrail.eu
 AppName=DogsOnTrailApp
 
 cd /etc/letsencrypt/live/dogsontrail.eu/
+cd /dogsontrail_data/certbot/conf/live/dogsontrail.eu/
 
 openssl pkcs12 -export -out ${DomainName}.pfx -inkey privkey.pem -in cert.pem -certfile fullchain.pem -passin pass: -passout pass:
 
-sudo docker secret create $SecretName /etc/letsencrypt/archive/$DomainName/${DomainName}.pfx
+sudo docker secret create $SecretName /dogsontrail_data/certbot/conf/live/dogsontrail.eu/${DomainName}.pfx
 
 sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName $AppName
-sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName dogsontrail_grpc
-sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName dogsontrail_webapi
+sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName DogsOnTrail.gRPC
+sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName DogsOnTrail.WebApi
 
 sudo docker secret rm $OldSecretName
