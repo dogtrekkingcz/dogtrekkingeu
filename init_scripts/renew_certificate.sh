32171d7ad7e914c11b6d12828b8824cb8387344e
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-05-16 16:24:14 +0200

    cleaning the renew certificates script

--- init_scripts/renew_certificate.sh
+++ init_scripts/renew_certificate.sh
@@ -1,18 +1,10 @@
-SecretName=$(date +%Y-%m-%d)
-OldSecretName=$(date --date yesterday +%Y-%m-%d)
 DomainName=dogsontrail.eu
-AppName=DogsOnTrailApp
 
-cd /dogsontrail_data/certbot/conf/live/dogsontrail.eu/
+cd /dogsontrail_data/certbot/conf/live/${DomainName}/
 
 openssl pkcs12 -export -out ${DomainName}.pfx -inkey privkey.pem -in cert.pem -certfile fullchain.pem -passin pass: -passout pass:
-chmod 775 /dogsontrail_data/certbot/conf/live/dogsontrail.eu/dogsontrail.eu.pfx
+chmod 775 /dogsontrail_data/certbot/conf/live/${DomainName}/${DomainName}.pfx
 
-sudo docker secret create $SecretName /dogsontrail_data/certbot/conf/live/dogsontrail.eu/${DomainName}.pfx
-
-sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName $AppName
-sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName DogsOnTrail.gRPC
-sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName DogsOnTrail.WebApi
-sudo docker service update --secret-add $SecretName --secret-rm $OldSecretName --env-add Kestrel__Certificates__Default__Path=/run/secrets/$SecretName DogsOnTrail.App
-
-sudo docker secret rm $OldSecretName
+cp /dogsontrail_data/certbot/conf/archive/dogsontrail.eu/fullchain2.pem /dogsontrail_data/certbot/conf/live/${DomainName}/fullchain.pem
+cp /dogsontrail_data/certbot/conf/archive/dogsontrail.eu/privkey2.pem /dogsontrail_data/certbot/conf/live/${DomainName}/privkey.pem
+chmod 755 -R /dogsontrail_data/certbot/conf/live/
