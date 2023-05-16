af6189b2635c86c8758255d48d25dd73209f375c
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-05-16 17:11:39 +0200

    certificates for keycloak also added

--- init_scripts/renew_certificate.sh
+++ init_scripts/renew_certificate.sh
@@ -7,4 +7,5 @@ chmod 775 /dogsontrail_data/certbot/conf/live/${DomainName}/${DomainName}.pfx
 
 cp /dogsontrail_data/certbot/conf/archive/dogsontrail.eu/fullchain2.pem /dogsontrail_data/certbot/conf/live/${DomainName}/fullchain.pem
 cp /dogsontrail_data/certbot/conf/archive/dogsontrail.eu/privkey2.pem /dogsontrail_data/certbot/conf/live/${DomainName}/privkey.pem
+
 chmod 755 -R /dogsontrail_data/certbot/conf/live/
