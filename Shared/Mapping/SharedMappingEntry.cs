59d8a3cb5ddb3855923003b4dc2154eb9a79cca1
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-07-01 12:10:03 +0200

    addd mapping of the entry-birthday to protos

--- Shared/Mapping/SharedMappingEntry.cs
+++ Shared/Mapping/SharedMappingEntry.cs
@@ -17,11 +17,13 @@ namespace SharedCode.Mapping
 
             typeAdapterConfig.NewConfig<EntryDto, Protos.Shared.Entry>()
                 .IgnoreNullValues(true)
-                .Map(d => d.Created, s => s.Created.ToGoogleDateTime());
+                .Map(d => d.Created, s => s.Created.ToGoogleDateTime())
+                .Map(d => d.Birthday, s => s.Birthday.Value.ToGoogleDateTime());
 
             typeAdapterConfig.NewConfig<Protos.Shared.Entry, EntryDto>()
                 .IgnoreNullValues(true)
-                .Map(d => d.Created, s => s.Created.ToDateTimeOffset());
+                .Map(d => d.Created, s => s.Created.ToDateTimeOffset())
+                .Map(d => d.Birthday, s => s.Birthday.ToDateTimeOffset());
 
             typeAdapterConfig.NewConfig<EntryDto.DogDto, Protos.Shared.EntryDog>()
                 .IgnoreNullValues(true)
