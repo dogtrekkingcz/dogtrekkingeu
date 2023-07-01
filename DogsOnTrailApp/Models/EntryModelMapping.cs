ab04b2354c6b7888906a4c867ee38fe494bbf846
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-07-01 10:54:43 +0200

    fixes when mapping

--- DogsOnTrailApp/Models/EntryModelMapping.cs
+++ DogsOnTrailApp/Models/EntryModelMapping.cs
@@ -1,5 +1,6 @@
 ﻿using SharedCode.Extensions;
 using Mapster;
+using SharedCode.Entities;
 
 namespace DogsOnTrailApp.Models;
 
@@ -19,7 +20,10 @@ internal static class EntryModelMapping
         typeAdapterConfig.NewConfig<EntryModel.DogDto, Protos.Shared.EntryDog>()
             .IgnoreNullValues(true)
             .Map(d => d.Birthday, s => s.Birthday != null ? s.Birthday.Value.ToGoogleDateTime() : null);
-
+        typeAdapterConfig.NewConfig<DogDto.VaccinationDto, Protos.Shared.Vaccination>()
+            .IgnoreNullValues(true)
+            .Map(d => d.Date, s => s.Date != null ? s.Date.Value.ToGoogleDateTime() : null);
+            
         typeAdapterConfig.NewConfig<EntryModel, Protos.Entries.CreateEntryRequest>()
             .Ignore(d => d.Entry.Id)
             .Ignore(d => d.Entry.Created);
