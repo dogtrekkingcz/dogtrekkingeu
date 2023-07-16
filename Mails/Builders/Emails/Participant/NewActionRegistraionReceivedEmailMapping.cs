a169a007a500537c07e9c059758f44370f12a002
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-07-16 15:52:02 +0200

    .

--- Mails/Builders/Emails/Participant/NewActionRegistraionReceivedEmailMapping.cs
+++ Mails/Builders/Emails/Participant/NewActionRegistraionReceivedEmailMapping.cs
@@ -1,11 +1,57 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Mails.Entities;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mails.Builders.Emails.Participant;
 
 public static class NewActionRegistraionReceivedEmailMapping
 {
-    public static IServiceCollection AddNewAtionRegistrationEmailMapping(this IServiceCollection services)
+    public static TypeAdapterConfig AddNewAtionRegistrationEmailMapping(this TypeAdapterConfig typeAdapterConfig)
     {
-        return services;
+        typeAdapterConfig.NewConfig<NewActionRegistrationEmailRequest, NewActionRegistrationEmailRequest>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.ActionDto, NewActionRegistrationEmailRequest.ActionDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.CategoryDto, NewActionRegistrationEmailRequest.CategoryDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.DogDto, NewActionRegistrationEmailRequest.DogDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.RacerDto, NewActionRegistrationEmailRequest.RacerDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.RaceDto, NewActionRegistrationEmailRequest.RaceDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.PaymentDto, NewActionRegistrationEmailRequest.PaymentDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.TermDto, NewActionRegistrationEmailRequest.TermDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+
+        typeAdapterConfig
+            .NewConfig<NewActionRegistrationEmailRequest.VaccinationDto, NewActionRegistrationEmailRequest.VaccinationDto>()
+            .IgnoreNullValues(true)
+            .TwoWays();
+        
+        return typeAdapterConfig;
     }
 }
