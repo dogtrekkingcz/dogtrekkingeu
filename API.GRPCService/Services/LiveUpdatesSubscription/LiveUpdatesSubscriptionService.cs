ac8203c2bc0043164e8f72004418c4191ffbe45b
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-07-26 15:59:12 +0200

    adding live updates subscription grpc

---
+++ API.GRPCService/Services/LiveUpdatesSubscription/LiveUpdatesSubscriptionService.cs
@@ -0,0 +1,13 @@
+﻿namespace API.GRPCService.Services.LiveUpdatesSubscription;
+
+public class LiveUpdatesSubscriptionService : Protos.LiveUpdatesSubscription.LiveUpdatesSubscription.LiveUpdatesSubscriptionBase
+{
+    public async override Task<Protos.LiveUpdatesSubscription.LiveUpdateSubscriptionResponse> addResult(Protos.LiveUpdatesSubscription.LiveUpdateSubscriptionRequest request, ServerCallContext context)
+    {
+        return new LiveUpdateSubscriptionResponse
+        {
+            
+
+        };
+    }
+}
