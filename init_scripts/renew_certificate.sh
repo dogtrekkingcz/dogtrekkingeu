d21e90081ee28749c6a6f047ca27007c4636f6fb
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-07-31 15:28:43 +0200

    updating dogs to pets

--- init_scripts/renew_certificate.sh
+++ init_scripts/renew_certificate.sh
@@ -1,11 +1,11 @@
-DomainName=dogsontrail.eu
+DomainName=petsontrail.eu
 
-cd /dogsontrail_data/certbot/conf/live/${DomainName}/
+cd /petsontrail_data/certbot/conf/live/${DomainName}/
 
 openssl pkcs12 -export -out ${DomainName}.pfx -inkey privkey.pem -in cert.pem -certfile fullchain.pem -passin pass: -passout pass:
-chmod 775 /dogsontrail_data/certbot/conf/live/${DomainName}/${DomainName}.pfx
+chmod 775 /petsontrail_data/certbot/conf/live/${DomainName}/${DomainName}.pfx
 
-cp /dogsontrail_data/certbot/conf/archive/dogsontrail.eu/fullchain2.pem /dogsontrail_data/certbot/conf/live/${DomainName}/fullchain.pem
-cp /dogsontrail_data/certbot/conf/archive/dogsontrail.eu/privkey2.pem /dogsontrail_data/certbot/conf/live/${DomainName}/privkey.pem
+cp /petsontrail_data/certbot/conf/archive/petsontrail.eu/fullchain2.pem /petsontrail_data/certbot/conf/live/${DomainName}/fullchain.pem
+cp /petsontrail_data/certbot/conf/archive/petsontrail.eu/privkey2.pem /petsontrail_data/certbot/conf/live/${DomainName}/privkey.pem
 
 chmod 755 -R /dogsontrail_data/certbot/conf/live/
