2a8f24b44f9d4d0b1289c6245fc974e6ba40059e
Radek Kotěšovec <kotesovec@cngroup.dk>
2023-08-04 17:49:54 +0200

    Note could be nulled and -> ignored

--- API.GRPCService/Services/Checkpoints/CheckpointsServiceMapping.cs
+++ API.GRPCService/Services/Checkpoints/CheckpointsServiceMapping.cs
@@ -7,7 +7,8 @@ internal static class CheckpointsServiceMapping
 {
     internal static TypeAdapterConfig AddCheckpointsServiceMapping(this TypeAdapterConfig typeAdapterConfig)
     {
-        typeAdapterConfig.NewConfig<Protos.Checkpoints.AddCheckpoint.AddCheckpointRequest, AddCheckpointItemRequest>();
+        typeAdapterConfig.NewConfig<Protos.Checkpoints.AddCheckpoint.AddCheckpointRequest, AddCheckpointItemRequest>()
+            .IgnoreNullValues(true);
         typeAdapterConfig.NewConfig<Google.Type.LatLng, AddCheckpointItemRequest.LatLngDto>();
         typeAdapterConfig.NewConfig<AddCheckpointItemResponse, Protos.Checkpoints.AddCheckpoint.AddCheckpointResponse>();
 
@@ -16,8 +17,10 @@ internal static class CheckpointsServiceMapping
         typeAdapterConfig.NewConfig<Google.Type.LatLng, GetCheckpointItemsRequest.LatLngDto>()
             .Map(d => d.Longitude, s => s.Longitude)
             .Map(d => d.Latitude, s => s.Latitude);
-        typeAdapterConfig.NewConfig<GetCheckpointItemsResponse, Protos.Checkpoints.GetCheckpoints.GetCheckpointsResponse>();
-        typeAdapterConfig.NewConfig<GetCheckpointItemsResponse.CheckpointItemDto, Protos.Checkpoints.GetCheckpoints.CheckpointItem>();
+        typeAdapterConfig.NewConfig<GetCheckpointItemsResponse, Protos.Checkpoints.GetCheckpoints.GetCheckpointsResponse>()
+            .IgnoreNullValues(true);
+        typeAdapterConfig.NewConfig<GetCheckpointItemsResponse.CheckpointItemDto, Protos.Checkpoints.GetCheckpoints.CheckpointItem>()
+            .IgnoreNullValues(true);
         typeAdapterConfig.NewConfig<GetCheckpointItemsResponse.LatLngDto, Google.Type.LatLng>();
             
         return typeAdapterConfig;
