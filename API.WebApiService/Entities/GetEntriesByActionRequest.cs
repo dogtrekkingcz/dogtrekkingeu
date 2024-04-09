ce515061d527e0aa81d188ee6c72590d2ec479d5
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-04-09 12:16:41 +0200

    fixing of mapping

--- API.WebApiService/Entities/GetEntriesByActionRequest.cs
+++ API.WebApiService/Entities/GetEntriesByActionRequest.cs
@@ -5,4 +5,6 @@ namespace API.WebApiService.Entities;
 public sealed record GetEntriesByActionRequest : IRequest<GetEntriesByActionResponse>
 {
     public string ActionId { get; set; }
+
+    public bool IncludeAlreadyAccepted { get; set; } = false;
 }
