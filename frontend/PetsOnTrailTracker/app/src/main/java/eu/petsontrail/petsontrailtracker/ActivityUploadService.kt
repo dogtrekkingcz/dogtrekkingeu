736129cdd51803e327ad74c43a7ab34c46266268
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-04-09 11:45:23 +0200

    Creating & updating activities using webapi/rest

---
+++ frontend/PetsOnTrailTracker/app/src/main/java/eu/petsontrail/petsontrailtracker/ActivityUploadService.kt
@@ -0,0 +1,12 @@
+package eu.petsontrail.petsontrailtracker
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ActivityUploadService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
