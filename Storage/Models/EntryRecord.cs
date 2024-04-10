7bca413d803edfc648b18510bb8be3b257f15316
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-04-10 14:09:04 +0200

    namings

--- Storage/Models/EntryRecord.cs
+++ Storage/Models/EntryRecord.cs
@@ -120,7 +120,7 @@ internal sealed record EntryRecord : IRecord
     {
         public string? Id { get; set; } = string.Empty;
 
-        public string FirstName { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
             
