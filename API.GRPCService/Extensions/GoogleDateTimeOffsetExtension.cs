1f07f9bc49a1fdfdcbcae31d68c7b63ca824774b
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-05-14 21:03:46 +0200

    --

--- API.GRPCService/Extensions/GoogleDateTimeOffsetExtension.cs
+++ API.GRPCService/Extensions/GoogleDateTimeOffsetExtension.cs
@@ -17,5 +17,32 @@
             
             return new DateTimeOffset(value.Seconds, TimeSpan.FromSeconds(value.Nanos));
         }
+
+        public static Google.Type.DateTime ToGoogleDateTime(this DateTimeOffset value)
+        {
+            return new Google.Type.DateTime
+            {
+                Year = value.Year,
+                Month = value.Month,
+                Day = value.Day,
+                Hours = value.Hour,
+                Minutes = value.Minute,
+                Seconds = value.Second
+            };
+        }
+
+        public static System.DateTimeOffset ToDateTimeOffsetNullable(this Google.Protobuf.WellKnownTypes.Timestamp? value)
+        {
+            if (value == null)
+                return System.DateTimeOffset.MinValue;
+
+            return new System.DateTimeOffset(value.Seconds, TimeSpan.FromSeconds(value.Nanos));
+        }
+
+        public static System.DateTime ToDateTime(this Google.Protobuf.WellKnownTypes.Timestamp value)
+        {
+            return new System.DateTime(value.Seconds, DateTimeKind.Utc);
+        }
+
     }
 }
