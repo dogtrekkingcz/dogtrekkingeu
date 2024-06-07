3a9530845aadb6f9c81599d6ff87587de724f1fa
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-06-07 13:11:46 +0200

    --

--- Storage/Entities/UserProfiles/UpdateUserProfileInternalStorageRequest.cs
+++ Storage/Entities/UserProfiles/UpdateUserProfileInternalStorageRequest.cs
@@ -2,146 +2,144 @@
 
 public sealed record UpdateUserProfileInternalStorageRequest
 {
-    public string? Id { get; set; } = string.Empty;
+    public string? Id { get; init; } = string.Empty;
 
-    public string UserId { get; set; } = string.Empty;
+    public string UserId { get; init; } = string.Empty;
 
-    public string Email { get; set; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
 
-    public Guid CompetitorId { get; set; } = default(Guid);
+    public Guid CompetitorId { get; init; } = default(Guid);
 
-    public string FirstName { get; set; } = string.Empty;
+    public string FirstName { get; init; } = string.Empty;
 
-    public string LastName { get; set; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
 
-    public string Nickname { get; set; } = string.Empty;
+    public string Nickname { get; init; } = string.Empty;
 
-    public DateTimeOffset? Birthday { get; set; } = null;
+    public DateTimeOffset? Birthday { get; init; } = null;
 
-    public AddressDto Address { get; set; } = new();
+    public AddressDto Address { get; init; } = new();
 
-    public ContactDto Contact { get; set; } = new();
+    public ContactDto Contact { get; init; } = new();
 
-    public List<PetDto> Pets { get; set; } = new List<PetDto>();
+    public List<PetDto> Pets { get; init; } = new List<PetDto>();
 
-    public List<string> Roles { get; set; } = new();
+    public List<string> Roles { get; init; } = new();
 
-    public List<ActivityDto> Activities { get; set; } = new();
+    public List<ActivityDto> Activities { get; init; } = new();
     
     public sealed record AddressDto
     {
-        public string Country { get; set; } = string.Empty;
+        public string Country { get; init; } = string.Empty;
 
-        public string Region { get; set; } = string.Empty;
+        public string Region { get; init; } = string.Empty;
 
-        public string ZipCode { get; set; } = string.Empty;
+        public string ZipCode { get; init; } = string.Empty;
 
-        public string City { get; set; } = string.Empty;
+        public string City { get; init; } = string.Empty;
 
-        public string Street { get; set; } = string.Empty;
+        public string Street { get; init; } = string.Empty;
 
-        public LatLngDto Position { get; set; } = new();
+        public LatLngDto Position { get; init; } = new();
     }
     
     public sealed record LatLngDto
     {
-        public double Latitude { get; set; } = 0.0;
+        public double Latitude { get; init; } = 0.0;
 
-        public double Longitude { get; set; } = 0.0;
+        public double Longitude { get; init; } = 0.0;
     }
     
     public sealed record ContactDto
     {
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string PhoneNumber { get; init; } = string.Empty;
 
-        public string EmailAddress { get; set; } = string.Empty;
+        public string EmailAddress { get; init; } = string.Empty;
     }
 
     public record PetDto
     {
-        public Guid Id { get; set; } = Guid.Empty;
+        public Guid Id { get; init; } = Guid.Empty;
 
-        public Guid UserId { get; set; } = Guid.Empty;
+        public Guid UserId { get; init; } = Guid.Empty;
 
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; init; } = string.Empty;
 
-        public string Kennel { get; set; } = string.Empty;
+        public string Kennel { get; init; } = string.Empty;
 
-        public string Pedigree { get; set; } = string.Empty;
+        public string Pedigree { get; init; } = string.Empty;
 
-        public string Chip { get; set; } = string.Empty;
+        public string Chip { get; init; } = string.Empty;
 
-        public DateTimeOffset? Birthday { get; set; } = null;
+        public DateTimeOffset? Birthday { get; init; } = null;
 
-        public DateTimeOffset? Decease { get; set; } = null;
+        public DateTimeOffset? Decease { get; init; } = null;
 
-        public string UriToPhoto { get; set; } = string.Empty;
+        public string UriToPhoto { get; init; } = string.Empty;
 
-        public string Contact { get; set; } = string.Empty;
+        public string Contact { get; init; } = string.Empty;
 
-        public Guid PetType { get; set; } = Guid.Empty;
+        public Guid PetType { get; init; } = Guid.Empty;
 
-        public List<VaccinationDto> Vaccinations { get; set; } = new List<VaccinationDto>();
+        public List<VaccinationDto> Vaccinations { get; init; } = new List<VaccinationDto>();
     }
 
     public sealed record VaccinationDto
     {
-        public DateTimeOffset? Date { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset? Date { get; init; } = DateTimeOffset.Now;
 
-        public DateTimeOffset? ValidUntil { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset? ValidUntil { get; init; } = DateTimeOffset.Now;
 
-        public Guid VaccinationType { get; set; } = Guid.Empty;
+        public Guid VaccinationType { get; init; } = Guid.Empty;
 
-        public string UriToPhoto { get; set; } = string.Empty;
+        public string UriToPhoto { get; init; } = string.Empty;
 
-        public string Note { get; set; } = string.Empty;
+        public string Note { get; init; } = string.Empty;
     }
 
     public sealed record ActivityDto
     {
-        public string? Id { get; set; }
+        public string? Id { get; init; }
 
-        public string UserId { get; set; } = string.Empty;
+        public Guid UserId { get; init; } = Guid.Empty;
+        public Guid ActionId { get; init; } = Guid.Empty;
+        public Guid RaceId { get; init; } = Guid.Empty;
+        public Guid CategoryId { get; init; } = Guid.Empty;
+        public Guid TypeId { get; init; } = Guid.Empty;
 
-        public string ActionId { get; set; } = Guid.Empty.ToString();
+        public string Name { get; init; } = string.Empty;
 
-        public string RaceId { get; set; } = Guid.Empty.ToString();
+        public string Description { get; init; } = string.Empty;
 
-        public string CategoryId { get; set; } = Guid.Empty.ToString();
+        public DateTimeOffset Start { get; init; } = DateTimeOffset.Now;
 
-        public string Name { get; set; } = string.Empty;
+        public DateTimeOffset? End { get; init; } = null;
 
-        public string Description { get; set; } = string.Empty;
+        public bool IsPublic { get; init; } = true;
 
-        public DateTimeOffset Start { get; set; } = DateTimeOffset.Now;
+        public List<PositionDto> Positions { get; init; } = new List<PositionDto>(0);
 
-        public DateTimeOffset? End { get; set; } = null;
-
-        public bool IsPublic { get; set; } = true;
-
-        public List<PositionDto> Positions { get; set; } = new List<PositionDto>(0);
-
-        public List<ActivityPetDto> Pets { get; set; } = new List<ActivityPetDto>(0);
+        public List<ActivityPetDto> Pets { get; init; } = new List<ActivityPetDto>(0);
     }
     public sealed record PositionDto
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Id { get; init; } = Guid.NewGuid().ToString();
 
-        public DateTimeOffset Time { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset Time { get; init; } = DateTimeOffset.Now;
 
-        public double Latitude { get; set; } = double.NaN;
+        public double Latitude { get; init; } = double.NaN;
 
-        public double Longitude { get; set; } = double.NaN;
+        public double Longitude { get; init; } = double.NaN;
 
-        public double Altitude { get; set; } = double.NaN;
+        public double Altitude { get; init; } = double.NaN;
 
-        public double Accuracy { get; set; } = double.NaN;
+        public double Accuracy { get; init; } = double.NaN;
 
-        public double Course { get; set; } = double.NaN;
+        public double Course { get; init; } = double.NaN;
 
-        public string Note { get; set; } = string.Empty;
+        public string Note { get; init; } = string.Empty;
 
-        public List<string> PhotoUris { get; set; } = new();
+        public List<string> PhotoUris { get; init; } = new();
     }
 
     public sealed record ActivityPetDto
