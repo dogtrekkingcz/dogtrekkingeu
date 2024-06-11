b58771d0d29187c68a92bf50ea5a441aeb187a47
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-06-11 07:28:41 +0200

    --

--- Storage/Models/BaseRecord.cs
+++ Storage/Models/BaseRecord.cs
@@ -7,9 +7,9 @@ internal record BaseRecord : IRecord
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string Id { get; set; } = string.Empty;
 
-    public Guid UserId { get; set; }
+    public Guid UserId { get; set; } = Guid.Empty;
 
-    public Guid CorrelationId { get; set; }
+    public Guid CorrelationId { get; set; } = Guid.Empty;
 }
