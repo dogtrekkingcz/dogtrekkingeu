f63d716bdda3f21d2cc830ce6f57453b38fff8bf
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-06-12 16:05:26 +0200

    --

--- Storage/Models/IRecord.cs
+++ Storage/Models/IRecord.cs
@@ -6,7 +6,6 @@ namespace Storage.Models;
 internal interface IRecord
 {
     [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
     public Guid Id { get; set; }
 
     public Guid UserId { get; set; }
