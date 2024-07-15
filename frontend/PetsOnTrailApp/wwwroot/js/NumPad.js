f5e4eec8c865031362e7586a507a37958fbd35c9
Radek Kotěšovec <radek.kotesovec@dogtrekking.cz>
2024-07-15 13:58:54 +0200

    Added numpad component

---
+++ frontend/PetsOnTrailApp/wwwroot/js/NumPad.js
@@ -0,0 +1,22 @@
+﻿window.initializeNumpad = (dotNetHelper) => {
+    document.querySelectorAll('input[type="text"]').forEach(input => {
+        input.addEventListener('focus', () => {
+            const rect = input.getBoundingClientRect();
+            const numpadOverlay = document.querySelector('.numpad-overlay');
+            numpadOverlay.style.display = 'block';
+            numpadOverlay.style.top = `${rect.bottom}px`;
+            numpadOverlay.style.left = '0';
+            numpadOverlay.style.right = '0';
+
+            dotNetHelper.invokeMethodAsync('ShowNumpad', input);
+        });
+    });
+};
+
+window.updateInputValue = (input, value) => {
+    input.value = value;
+};
+
+window.hideNumpad = () => {
+    document.querySelector('.numpad-overlay').style.display = 'none';
+};
